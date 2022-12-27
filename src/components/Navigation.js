d2c1ba6d236f352604471f89a1abbe6ffa37a461
ElijahBcodes <eli1inthesnow@gmail.com>
2022-12-27 15:13:44 -0800

    navbar works, needs links and styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import { Link, useLocation ,} from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { FcMenu } from "react-icons/fc";
+import {NavLink} from 'react-router-dom'
 
 const Navigation = () => {
     const [expandNavbar, setExpandNavbar] = useState(false);
@@ -13,20 +14,26 @@ const Navigation = () => {
 
 
     return (
-        <div className='m:h-9 shadow-md w-full top-0 left-0'  id={expandNavbar ? "open" : "close"}>
-            <div className='toggleButton'>
-                <button
-                    onClick={() => {
-                        setExpandNavbar((prev) => !prev);
-                    }}
-                >
-                    {/* <FcMenu /> */}
-                </button>
-            </div>
-            <div className=''>
-                <Link to="/"> Home </Link>
-                <br></br>
-                <Link to="/About"> About </Link>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+            <NavLink to="/" className="btn btn-ghost normal-case text-xl">Home
+            </NavLink>
+        </div>
+        <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+            <li><NavLink to='/about'>About</NavLink></li>
+            <li tabIndex={0}>
+                <a>
+                Resume
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+                </a>
+                <ul className="p-2 bg-base-100">
+                <li><a>Projects</a></li>
+                <li><a>Skills</a></li>
+                </ul>
+            </li>
+            <li><a>Item 3</a></li>
+            </ul>
             </div>
         </div>
     )
