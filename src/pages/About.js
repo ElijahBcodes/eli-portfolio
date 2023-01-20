d00600680624680e71c384c684038cd64306ede6
ElijahBcodes <eli1inthesnow@gmail.com>
2023-01-19 16:14:56 -0800

    fixed indents

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,15 +6,14 @@ const About = () => {
     <>
       <div className="card w-98 bg-base-100 shadow-xl m-2">
         <figure className="h-50">
-          <img src={pic1}alt="profile pic" />
+          <img src={pic1} alt="profile pic" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">About Me</h2>
-          <p>My name is Elijah, a full stack web
-          developer specializing in React and Ruby on Rails. Thank you for visiting my portfolio! </p>
-          {/* <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div> */}
+          <p>
+            My name is Elijah, a full stack web developer specializing in React
+            and Ruby on Rails. Thank you for visiting my portfolio!{" "}
+          </p>
         </div>
       </div>
     </>
