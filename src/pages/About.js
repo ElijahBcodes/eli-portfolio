a75b1401d9ecbbf1eb0414f0e869cb44c3a9fbd4
ElijahBcodes <eli1inthesnow@gmail.com>
2023-01-19 16:13:12 -0800

    about page added card, home page font

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,20 @@
 import React from "react";
+import pic1 from "../assets/portrait.png";
 
 const About = () => {
   return (
     <>
-      <h1>Hello, My name is Elijah </h1>
-      <div className="avatar">
-        <div className="max-w-sm rounded-lg ">
-          <img src={pic1} alt="profile pic" />
-        </div>
-        <div>
-          {""}Hello and welcome! My name is Elijah, and I am a full stack web
-          developer specializing in React and Ruby on Rails. With the completion of a full-stack coding
-          bootcamp along with continuous hours of study and hands on training
-          over the past year, I have developed a strong skillset in a variety of
-          technologies and approaches, allowing me to tackle a wide range of
-          projects and challenges. When I'm not coding, you can find me hitting
-          the trails for a run or scaling a mountain. I believe that a balance
-          between work and play is essential for both personal and professional
-          growth, and I am always looking for new ways to challenge myself and
-          improve my skills. Thank you for visiting my profile, and I look
-          forward to working with you!
+      <div className="card w-98 bg-base-100 shadow-xl m-2">
+        <figure className="h-50">
+          <img src={pic1}alt="profile pic" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">About Me</h2>
+          <p>My name is Elijah, a full stack web
+          developer specializing in React and Ruby on Rails. Thank you for visiting my portfolio! </p>
+          {/* <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div> */}
         </div>
       </div>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import Pic from "../assets/portrait.png";
 function Home() {
   return (
     <>
-      <div className="bg-slate-200 bg-fill">
-        <h1 className="font-serif"> Elijah Barreto</h1>
-        <h2>Full Stack Web Developer</h2>
+      <div className="bg-transparent bg-fill">
+        <h1 className="font-serif text-center"> Elijah Barreto</h1>
+        <h2 className="font-serif text-center">Full Stack Web Developer</h2>
         <div className="container mx-auto px-4">
           <img className="mask mask-circle" alt="portrait" src={Pic} />
         </div>
