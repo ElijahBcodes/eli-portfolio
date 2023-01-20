ec867f491fc496109ab42b04fc112752dda76b71
ElijahBcodes <eli1inthesnow@gmail.com>
2023-01-20 15:02:53 -0800

    added particles to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Pic from "../assets/portrait.png";
+import ParticlesBackground from "../components/ParticlesBackground"
 
 function Home() {
   return (
     <>
-      <div className="bg-transparent bg-fill">
+    
+      <div className=""><ParticlesBackground />
         <h1 className="font-serif text-center"> Elijah Barreto</h1>
         <h2 className="font-serif text-center">Full Stack Web Developer</h2>
         <div className="container mx-auto px-4">
           <img className="mask mask-circle" alt="portrait" src={Pic} />
         </div>
-        <p> A React Developer with a passion for learning and technology.</p>
+          <p> A React Developer with a passion for learning and technology.</p>
       </div>
+      
     </>
   );
 }
