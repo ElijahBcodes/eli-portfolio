2de4afb5f4a288f5df7aa87356c3cb1c1bd493b4
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-02-01 16:21:56 -0800

    format , removed commented code,

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,19 @@
 import React from "react";
 import Pic from "../assets/portrait.png";
-import ParticlesBackground from "../components/ParticlesBackground"
+import ParticlesBackground from "../components/ParticlesBackground";
 
 function Home() {
   return (
     <>
-    
-      <div className="relative z-100"><ParticlesBackground />
+      <div className="relative z-100">
+        <ParticlesBackground />
         <h1 className="font-serif text-center"> Elijah Barreto</h1>
         <h2 className="font-serif text-center">Full Stack Web Developer</h2>
         <div className="container mx-auto px-4">
           <img className="mask mask-circle" alt="portrait" src={Pic} />
         </div>
-          <p> A React Developer with a passion for learning and technology.</p>
+        <p> A React Developer with a passion for learning and technology.</p>
       </div>
-      
     </>
   );
 }
