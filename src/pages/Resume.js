2a803fef26c5035e4d2a09c357da91d8d488fe31
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-02-21 15:31:22 -0800

    indent fix

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-import Doc from '../ElijahBarreto.resume.pdf'
-
+import React from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import Doc from "../ElijahBarreto.resume.pdf";
 
 export default function Resume() {
-
   return (
     <>
-    <div className='hero bg-black'>
-      <div className='hero-content  justify-center '>
-    <Document file={Doc} >
-      <Page pageNumber={1} />
-    </Document>
+      <div className="hero bg-black">
+        <div className="hero-content  justify-center ">
+          <Document file={Doc}>
+            <Page pageNumber={1} />
+          </Document>
+        </div>
       </div>
-  </div>
-</>
-  )
+    </>
+  );
 }
-
