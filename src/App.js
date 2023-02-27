fcb4d174c6beefe5d8aaa9018cb2cdd02ff0aaae
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-02-26 21:05:15 -0800

    updated theme changer in navigation. works, need to change styles for light and dark text

--- src/App.js
+++ src/App.js
@@ -8,30 +8,22 @@ import Resume from "./pages/Resume";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import { useEffect } from 'react'
-import { themeChange } from 'theme-change'
 
 const App = () => {
-  useEffect(() => {
-    themeChange(false)
-    // 👆 false parameter is required for react project
-  }, [])
   return (
-    <div>
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/project/:id" element={<ProjectDisplay />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project/:id" element={<ProjectDisplay />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,57 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
+    },
   },
-},
-  plugins:[require('flowbite/plugin'),
-  require("daisyui"),
-]
-}
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#1182d8",
+
+          secondary: "#a1fc9c",
+
+          accent: "#00ce2c",
+
+          neutral: "#282031",
+
+          "base-100": "#F0EFF5",
+
+          info: "#67ADE9",
+
+          success: "#1B6F66",
+
+          warning: "#EBBB37",
+
+          error: "#EA3E69",
+        },
+        dark: {
+          primary: "#4c3da5",
+
+          secondary: "#5238af",
+
+          accent: "#0f2791",
+
+          neutral: "#192024",
+
+          "base-100": "#282B43",
+
+          info: "#5387D0",
+
+          success: "#54DEBE",
+
+          warning: "#F0B133",
+
+          error: "#F96C71",
+        },
+      },
+    ],
+  },
+  plugins: [require("flowbite/plugin"), require("daisyui")],
+};
